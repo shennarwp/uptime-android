71ef7735fa00e3611eef51293577f1c7a88b393d
Shenna Risqianto Wilfred Piri <shenna.piri@fourenergy.de>
2026-09-25 19:55:07 +0200

    fix(ci): use AGP built-in Kotlin coverage classes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ tasks.register<JacocoReport>("jacocoDebugTestReport") {
         fileTree(layout.buildDirectory) {
             include("tmp/kotlin-classes/debug/**/*.class")
             include("intermediates/javac/debug/**/*.class")
+            include("intermediates/built_in_kotlinc/debug/compileDebugKotlin/classes/**/*.class")
             include("intermediates/compile_library_classes_jar/debug/**/*.jar")
             include("intermediates/runtime_library_classes_jar/debug/**/*.jar")
             exclude(
