625503d439308c7c1800477fe5a6b213c1fbe163
Shenna Risqianto Wilfred Piri <shenna.piri@fourenergy.de>
2026-09-25 14:37:27 +0200

    fix(ci): generate usable JaCoCo coverage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            enableUnitTestCoverage = true
+        }
         release {
             optimization {
                 enable = false
